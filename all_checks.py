314781027903da6c06f879941dfc88bed4384411
Oliver Fragoso <oliverfra27@gmail.com>
2020-10-28 09:55:03 -0600

    Add a simple CPU load check

--- all_checks.py
+++ all_checks.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import sys
 import socket
+import psutil
 
 def check_reboot():
 	"""Returns True if the Computer has a pending reboot"""
@@ -24,6 +25,10 @@ def check_root_full():
 	"""Returns True if the root partition is full, False otherwise"""
 	return check_disk_full("/",2,10)
 
+def check_cpu_constrained():
+	"""Returns True if the cpu is having too much usage, False otherwise"""
+	return psutil.cpu.percent(1) > 75
+
 def check_no_network():
 	"""Returns True if it fails to resolve Google's URL, False otherwise"""
 
@@ -37,6 +42,7 @@ def main():
 	checks=[
 		(check_reboot,"Pending Reboot"),
 		(check_root_full,"Root partition full"),
+		(check_cpu_constrained,"CPU load too high")
 		(check_no_network,"No working,network")
 	]
 	everything_ok = True
